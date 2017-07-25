0e0c5099bb9c2131e2964a0ee87b554f5df84723
Tyson Rasmussen <tyson.rasmussen@motorolasolutions.com>
2017-07-25 12:37:44 -0600

    fix index

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Hello again WORLD!</h1>
+                <h1>Hello again Tyson!</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
